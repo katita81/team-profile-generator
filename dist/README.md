c1cb43652e0b9aab196f2971b40fd2cf7b0948fe
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-02-01 18:33:57 +1100

    created directories and relevant files, started html code, add Employee class, installed inquirer

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+include link to the walkthrough video
---
+++ index.js
@@ -0,0 +1,13 @@
+// ask the user fore manger info
+// then
+// -- use that info to create a manager obj
+// -- store that ojb
+// -- ask the user if they want to create a intern eng or quit
+// -- then
+// -- -- if they chose create
+// -- -- -- ask them for emp info
+// -- -- -- create the appropt obj
+// -- -- -- stasch that obj w/ the others
+// -- -- -- repeat *  ask the user if they want to create a intern eng or quit
+// -- -- else
+// -- -- -- take you collection of emps and create an html file
